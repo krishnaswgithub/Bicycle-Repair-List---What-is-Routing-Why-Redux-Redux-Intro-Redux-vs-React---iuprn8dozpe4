f590df60e403965858305184039e73700fb4d38e
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-17 19:59:31 +0530

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -19,3 +19,81 @@ const reducer = (state = [],action = {}) => {
   
 export default reducer;
 */
+
+const initialState = {
+  items: [],
+  item: {
+    id: 0,
+    owner: '',
+    model: '',
+    description: '',
+  },
+  editMode: false,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'repairAdded':
+      return {
+        ...state,
+        items: [
+          ...state.items,
+          {
+            id: state.items.length + 1,
+            owner: action.payload.owner,
+            model: action.payload.model,
+            description: action.payload.description,
+            resolved: false,
+          },
+        ],
+      };
+    case 'repairRemoved':
+      return {
+        ...state,
+        items: state.items.filter((repair) => repair.id !== action.payload.id),
+      };
+    case 'repairResolved':
+      return {
+        ...state,
+        items: state.items.map((repair) =>
+          repair.id === action.payload.id ? { ...repair, resolved: !repair.resolved } : repair
+        ),
+      };
+    case 'repairUpdated':
+      return {
+        ...state,
+        items: state.items.map((repair) =>
+          repair.id === action.payload.id
+            ? {
+                ...repair,
+                owner: action.payload.owner,
+                model: action.payload.model,
+                description: action.payload.description,
+              }
+            : repair
+        ),
+        item: {
+          id: 0,
+          owner: '',
+          model: '',
+          description: '',
+        },
+        editMode: false,
+      };
+    case 'editTask':
+      return {
+        ...state,
+        item: {
+          id: action.payload.id,
+          owner: action.payload.owner,
+          model: action.payload.model,
+          description: action.payload.description,
+        },
+        editMode: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
